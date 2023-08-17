67a802c5a5094d3531333b3870626987f6865f92
Manuela <mcastanof1@eafit.edu.co>
2023-08-16 23:35:47 -0500

    sprint1

--- book/models.py
+++ book/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    description= models.CharField(max_length=250)
+    cover= models.ImageField(upload_to = 'book/images/')
+    
+    
+class Reader(models.Model):
+    name = models.CharField(max_length=100)
+    profile_pic= models.ImageField(upload_to = 'book/images/')
+    
+
+
--- book/views.py
+++ book/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Book
+from .models import Reader
+
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def recomendations(request):
+    books = Book.objects.all()
+    return render(request, 'recomendations.html', {'books': books})
+
+def profile(request):
+    books = Book.objects.all()
+    reader = Reader.objects.first()
+    return render(request, 'profile.html', {'books': books, 'reader': reader})
+
