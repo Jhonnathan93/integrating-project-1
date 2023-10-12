5bd7926c86bd3ce0ee9f9f2b1f349d2f32b898dd
santiagoneusa <ruizsantiago836@gmail.com>
2023-10-12 16:27:02 -0500

    crear nuevo modelo en accounts para almacenar los datos de usuario

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    # Agrega campos personalizados
+    gender = models.CharField(max_length=10, blank=True)
+    birthdate = models.DateField(null=True, blank=True)
+    profile_picture = models.ImageField(upload_to='accounts/profile_pics/', null=True, blank=True)
