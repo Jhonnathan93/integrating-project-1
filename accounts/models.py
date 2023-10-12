34315cda9cba783b2eea7794c0833f3b97e052ba
santiagoneusa <ruizsantiago836@gmail.com>
2023-10-12 16:39:52 -0500

    creación de archivo gitignore y keys.env

--- accounts/models.py
+++ accounts/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
 
-class CustomUser(AbstractUser):
-    # Agrega campos personalizados
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     gender = models.CharField(max_length=10, blank=True)
     birthdate = models.DateField(null=True, blank=True)
-    profile_picture = models.ImageField(upload_to='accounts/profile_pics/', null=True, blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
